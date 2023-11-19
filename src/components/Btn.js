1918b095ef553224dd777aadac4d8243a8f2fdea
parvejg <parvezpg321@gmail.com>
2023-11-19 13:54:20 +0530

    add link btns

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,7 +1,18 @@
+import "./Button.css";
 export const Button = (btnName) => {
   return (
-    <div className="dropdowns-btns-wrapper">
-      <button>{btnName}</button>
-    </div>
+    <>
+      <button
+        className="click-btn"
+        btnName={btnName}
+        onClick={() => console.log()}
+      >
+        Click me
+      </button>
+
+      <button className="dropdown-link">Link 1</button>
+      <button className="dropdown-link">Link 2</button>
+      <button className="dropdown-link">Link 3</button>
+    </>
   );
 };
