55922dc92732a07332d32400a0ee7c618b21aef9
parvejg <parvezpg321@gmail.com>
2023-11-19 12:21:54 +0530

    btn component ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "./components/Btn";
 
 function App() {
   return (
     <div className="App">
       <div className="main-wrapper">
         <h1>Click Dropdowns</h1>
+        <Button btnName="Click me" />
       </div>
       <h1>hello</h1>
     </div>
---
+++ src/components/Btn.js
@@ -0,0 +1,7 @@
+export const Button = (btnName) => {
+  return (
+    <div className="dropdowns-btns-wrapper">
+      <button>{btnName}</button>
+    </div>
+  );
+};
