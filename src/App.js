10fab3b48eeb5d20ecafdd6a3630fd85d6f13e7d
parvejg <parvezpg321@gmail.com>
2023-11-19 12:10:46 +0530

    heading dropdowns

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="main-wrapper">
+        <h1>Click Dropdowns</h1>
+      </div>
       <h1>hello</h1>
     </div>
   );
